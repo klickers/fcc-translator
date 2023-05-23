ff8468d859a6af64e1d52c06d21aca4e45ba4b4c
klickers <bluebirid228@gmail.com>
2023-05-23 12:06:51 -0700

    add translator.amToBritSpelling() function

--- components/translator.js
+++ components/translator.js
@@ -3,6 +3,25 @@ const americanToBritishSpelling = require("./american-to-british-spelling.js");
 const americanToBritishTitles = require("./american-to-british-titles.js");
 const britishOnly = require("./british-only.js");
 
-class Translator {}
+class Translator {
+    translate(text) {
+        text = this.amToBritSpelling(text);
+        return text;
+    }
+
+    amToBritSpelling(text) {
+        let str = text.split(" ");
+        for (let i = 0; i < str.length; i++) {
+            let spelling = americanToBritishSpelling[str[i].toLowerCase()];
+            if (spelling) {
+                if (str[i] != str[i].toLowerCase())
+                    str[i] =
+                        spelling.charAt(0).toUpperCase() + spelling.slice(1);
+                else str[i] = spelling;
+            }
+        }
+        return str.join(" ");
+    }
+}
 
 module.exports = Translator;
--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -2,5 +2,14 @@ const chai = require("chai");
 const assert = chai.assert;
 
 const Translator = require("../components/translator.js");
+let translator = new Translator();
 
-suite("Unit Tests", () => {});
+suite("Unit Tests", () => {
+    test("Translate Mangoes are my favorite fruit. to British English", function (done) {
+        assert.equal(
+            translator.translate("Mangoes are my favorite fruit."),
+            "Mangoes are my favourite fruit."
+        );
+        done();
+    });
+});
